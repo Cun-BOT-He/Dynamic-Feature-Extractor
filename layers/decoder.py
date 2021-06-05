fde95e2e7f6a486ec3b50ffff6ae678d4f3277e9
Bobby Ho <3150100127@zju.edu.cn>
2021-06-06 01:39:04 +0800

    Update decoder.py

--- layers/decoder.py
+++ layers/decoder.py
@@ -3,17 +3,17 @@
 
 import torch
 import torch.nn as nn
-import torch.nn.funtional as F
+import torch.nn.functional as F
 
 class DecoderRNN(nn.Module):
-    def __init__(self, Datainput_size : int, Encoderoutput_size : int, seq_len : int):
+    def __init__(self, input_size : int, hidden_size : int, seq_len : int):
         super(DecoderRNN, self).__init__()
-        self.hidden_size = Datainput_size
-        self.input_size = Encoderoutput_size
+        self.hidden_size = hidden_size
+        self.input_size = input_size
         self.seq_len = seq_len
-        self.gru = nn.GRU(Encoderoutput_size, self.hidden_size)
+        self.gru = nn.GRU(input_size, hidden_size)
 
-    def forward(self, input, hidden=None):
+    def forward(self, input_seq, hidden=None):
         """
         Args:
         input_seq : Tensor, shape of (T,N,D), 
@@ -23,9 +23,9 @@ class DecoderRNN(nn.Module):
         if len(input_seq.shape) == 2:
             input_seq = torch.unsqueeze(input_seq, 1)
         if hidden == None:
-            hidden = initHidden(input.shape[1], input.device)
-        output, _ = self.gru(input.view(self.seq_len, -1, self.input_size), hidden)
-        output = output.view(self.seq_len, -1, self.input_size)
+            hidden = self.initHidden(input_seq.shape[1], input_seq.device)
+        output, _ = self.gru(input_seq.view(self.seq_len, -1, self.input_size), hidden)
+        output = output.view(self.seq_len, -1, self.hidden_size)
         return output
     def initHidden(self, batch_size, device):
         return torch.zeros((1, batch_size, self.hidden_size), device=device)
